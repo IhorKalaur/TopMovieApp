f1ba5d58ce353c2067e2953c2f92d32d7557bd2b
KIM <KIM@d.c>
2024-02-23 12:55:37 +0200

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+w
+Gapp/src/main/java/ihor/kalaur/topmovieapp/ui/screens/MovieListScreen.kt,4\7\47d571bceaa22b8eafc41c8e56799c75d4e9b169
+k
+;app/src/main/java/ihor/kalaur/topmovieapp/ui/theme/Theme.kt,0\4\04ec5e699fa77156e63a8e76e7a84648ae4cf0f0
+D
+app/build.gradle.kts,d\2\d2a4383452aad84a18429a5844d31330a012a9e6
+i
+9app/src/main/java/ihor/kalaur/topmovieapp/di/AppModule.kt,8\f\8f01688d922a7c2c1023d5f7c8ad31a23fbbb992
+@
+build.gradle.kts,d\b\dbcff70658daf80b53ce624f6adcaa529df5ed8d
---
+++ app/src/main/java/ihor/kalaur/topmovieapp/MainActivity.kt
@@ -0,0 +1,46 @@
+package ihor.kalaur.topmovieapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ihor.kalaur.topmovieapp.ui.theme.TopMovieAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TopMovieAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TopMovieAppTheme {
+        Greeting("Android")
+    }
+}
